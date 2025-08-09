76890cb0a27571674d278b7743c6a26260d05967
nataraz <natarazjavaarena@gmail.com>
2025-08-09 09:57:19 +0530

    TL  modified sum method

--- GITProj01/src/main/java/com/nt/main/Arithmetic.java
+++ GITProj01/src/main/java/com/nt/main/Arithmetic.java
@@ -3,7 +3,9 @@ package com.nt.main;
 public class Arithmetic {
 
 	public  double sum(double x,double y) {
-		return x+y;
+		double z=0.0;
+		z=x+y;
+		return z;
 	}
 	
 	public    double  sub(double x,double y) {
