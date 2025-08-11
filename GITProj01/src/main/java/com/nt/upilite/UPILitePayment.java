8fde629ff2d632a9d0e82d5e17b2788da76f83fe
nataraz <natarazjavaarena@gmail.com>
2025-08-11 18:28:39 +0530

    TL Dev1 --PhonepeLite

---
+++ GITProj01/src/main/java/com/nt/upilite/UPILitePayment.java
@@ -0,0 +1,10 @@
+package com.nt.upilite;
+
+public class UPILitePayment {
+	
+	public  String doPaymentUsingPhonePeLite(long phoneno, double amount) {
+		return amount+" payment is happending using  phonepelite ";
+	}
+	
+
+}
