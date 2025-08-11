8ec71a0255c91d6667bcb7e2d46984edb60306b1
nataraz <natarazjavaarena@gmail.com>
2025-08-11 17:52:29 +0530

    Dev2  CardPayment

--- GITProj01/src/main/java/com/nt/card/CardPayment.java
+++ GITProj01/src/main/java/com/nt/card/CardPayment.java
@@ -5,5 +5,10 @@ public class CardPayment {
 	public String paymentUsingCreditCard(long cardno, double amount) {
 		return  amount+" amount payment is done using creditCard";
 	}
+	
+	public String paymentUsingDebitCard(long cardno, double amount) {
+		return  amount+" amount payment is done using DebitCard";
+	}
+	
 
 }
