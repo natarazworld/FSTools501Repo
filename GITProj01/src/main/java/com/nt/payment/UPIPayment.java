fa5470877552067069f7c7abce6911c444594bea
nataraz <natarazjavaarena@gmail.com>
2025-08-11 17:35:32 +0530

    Dev1 performing UPI Payment

---
+++ GITProj01/src/main/java/com/nt/payment/UPIPayment.java
@@ -0,0 +1,11 @@
+package com.nt.payment;
+
+public class UPIPayment {
+	
+	
+	public  String    doPaymentUsingPhonePe(long phoneno, double amount) {
+		return amount+"payment is  happening  from the phone number ::"+phoneno;
+		
+	}
+
+}
